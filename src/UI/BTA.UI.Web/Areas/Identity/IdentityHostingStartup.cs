2d54c83e1bda386bb5e4404855e04dc2e70f0e0e
Cherokee <simeonov23@yahoo.com>
2018-12-26 21:17:11 +0200

    Scaffolded Identity before switch to MVC pattern

---
+++ src/UI/BTA.UI.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using BTA.Data;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+
+//[assembly: HostingStartup(typeof(BTA.UI.Web.Areas.Identity.IdentityHostingStartup))]
+//namespace BTA.UI.Web.Areas.Identity
+//{
+//    public class IdentityHostingStartup : IHostingStartup
+//    {
+//        public void Configure(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureServices((context, services) => {
+//            });
+//        }
+//    }
+//}
