8499b707adc18e53a5e0393c139eb1511f9f4101
Cherokee <simeonov23@yahoo.com>
2018-12-30 12:07:13 +0200

    Added Repository interface in AppLayer

---
+++ src/Core/BTA.Core.ApplicationServices/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace BTA.Core.ApplicationServices.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    public interface IRepository<T> where T : class
+    {
+        T GetById(int id);
+
+        IEnumerable<T> List();
+
+        IEnumerable<T> List(Expression<Func<T, bool>> predicate);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Edit(T entity);
+    }
+}
